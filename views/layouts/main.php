9629ef49315bab0d37bcfef27616dd56b15b4660
Shuk <10324899+shukrisenawi@users.noreply.github.com>
2026-07-29 17:23:24 +0800

    Buang butang go-top lama di footer, kekalkan butang back-to-top hijau baharu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -413,9 +413,6 @@ JS);
         <footer class="site-footer">
             <div class="text-center">
                 2019 &copy; MultiVita2u.com
-                <a href="#" class="go-top">
-                    <i class="fa fa-angle-up"></i>
-                </a>
             </div>
         </footer>
         <!--footer end-->
