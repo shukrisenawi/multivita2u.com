5fdadfb7cc51eba14155d69acaa02a9d8951b913
Shuk <10324899+shukrisenawi@users.noreply.github.com>
2026-07-29 16:57:08 +0800

    Redesign halaman senarai stokis: page hero, kad HQ, carian pantas stokis dan kad ejen moden mengikut negeri

--- views/site/agen.php
+++ views/site/agen.php
@@ -2,166 +2,160 @@
 
 use app\models\User;
 use yii\helpers\Url;
+use yii\helpers\Html;
+
+$this->title = 'Senarai Stokis';
+
+$levels = [
+    2 => ['label' => 'Stokis Negeri', 'icon' => 'fas fa-flag'],
+    3 => ['label' => 'Stokis', 'icon' => 'fas fa-store'],
+    4 => ['label' => 'Mobile Stokis', 'icon' => 'fas fa-truck-fast'],
+];
 ?>
-<section class="section section-text-light section-background section-center" style="background-image: url(images/header_text.png);">
-    <div class="container-fluid">
-        <div class="row align-items-center">
 
-            <div class="col">
-                <div class="row">
-                    <div class="col-md-12 align-self-center p-static order-2 text-center">
-                        <div class="overflow-hidden pb-2">
-                            <h1 class="text-dark font-weight-bold text-9 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="100">Senarai Stokis</h2>
-                        </div>
-                    </div>
-                    <div class="col-md-12 align-self-center order-1">
-                        <ul class="breadcrumb d-block text-center appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="300">
-                            <li><a href="index.php">Laman Utama</a></li>
-                            <li class="active">Stokis</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+<!-- ============ PAGE HERO ============ -->
+<section class="mv-page-hero">
+    <div class="mv-page-hero__grid-lines" aria-hidden="true"></div>
+    <div class="mv-page-hero__glow" aria-hidden="true"></div>
 
+    <div class="mv-container mv-page-hero__inner">
+        <span class="mv-hero__eyebrow">
+            <span class="mv-hero__eyebrow-dot"></span>
+            Rangkaian Pengedar
+        </span>
+        <h1 class="mv-page-hero__title">
+            Cari stokis<br><span>berhampiran anda</span>
+        </h1>
+        <p class="mv-page-hero__lead">
+            Dapatkan Multivita Milk daripada stokis bertauliah di seluruh Malaysia, Singapura &amp; Brunei.
+        </p>
+        <div class="mv-page-hero__chips">
+            <div class="mv-hero__chip"><strong>45K+</strong><span>Pengedar Aktif</span></div>
+            <div class="mv-hero__chip"><strong>3</strong><span>Negara</span></div>
+            <div class="mv-hero__chip"><strong>HQ</strong><span>012-954 4847</span></div>
         </div>
     </div>
 </section>
-<section class="page-section-ptb">
-    <div class="container">
-        <div class="testimonial-widget gray-bg p-4 rounded font-italic position-relative">
-            <div class="row">
-                <div class="col-lg-4 col-sm-12 sm-mb-30">
-                    <div class="card border-radius-0 bg-color-light border-0 box-shadow-1">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-4 col-lg-4"><img src="images/logo_ok.png" height="70" /></div>
-                                <div class="col-sm-6 col-lg-6">
-                                    <div class="team-info">
-                                        <h4><a href="#">HEADQUATERS</a></h4>
-                                    </div>
-                                    <div class="team-contact">
-                                        <span class="call"><i class="fa fa-phone"></i> &nbsp;012-9544847</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+<!-- ============ SENARAI STOKIS ============ -->
+<section class="mv-agen">
+    <div class="mv-container">
+
+        <!-- Carian -->
+        <div class="mv-agen__toolbar">
+            <div class="mv-agen__search">
+                <i class="fas fa-search"></i>
+                <input type="text" id="mvAgenSearch" placeholder="Cari nama, bandar atau negeri..." autocomplete="off">
             </div>
         </div>
-        <?php
-        $i = 1;
-        foreach ($state as $value) {
 
-        ?>
-            <hr class="pattern tall py-3">
-            <div class="col-lg-6 pe-lg-5 mb-5 mb-lg-0">
-                <div class="overflow-hidden">
-                    <h2 class="text-color-primary font-weight-semibold text-3 line-height-7 positive-ls-2 mb-0 appear-animation" data-appear-animation="maskUp">NEGERI </h2>
-                </div>
-                <div class="overflow-hidden mb-3">
-                    <h3 class="text-color-dark font-weight-bold text-transform-none line-height-1 text-10 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="200"><?= strtoupper($value->state) ?></h3>
-                </div>
+        <!-- HQ -->
+        <div class="mv-hq">
+            <img src="images/logo_ok.png" alt="Multivita HQ" class="mv-hq__logo">
+            <div class="mv-hq__info">
+                <span class="mv-hq__badge">Ibu Pejabat</span>
+                <strong>HEADQUATERS</strong>
+                <a href="tel:0129544847"><i class="fas fa-phone"></i> 012-954 4847</a>
             </div>
+            <a href="tel:0129544847" class="mv-btn mv-btn--ink mv-hq__btn">
+                <i class="fas fa-phone"></i> Hubungi HQ
+            </a>
+        </div>
 
-            <div class="testimonial-widget gray-bg p-4 rounded font-italic position-relative">
-                <?php $agen1[$i] = User::find()->where('(state=:state AND level_id=:levelId ) AND UPPER(name)<>"HEADQUATERS"', [':state' => $value->state, ':levelId' => 2])->all();
-                if ($agen1[$i]) { ?>
+        <div class="mv-agen__empty" id="mvAgenEmpty" style="display:none">
+            <i class="fas fa-magnifying-glass"></i>
+            <p>Tiada stokis ditemui. Cuba kata kunci lain.</p>
+        </div>
 
-                    <h4><i class="fa fa-arrow-circle-right"></i> &nbsp;&nbsp;STOKIS NEGERI</h4>
-                    <div class="divider divider-primary">
-                        <i class="fas fa-chevron-down"></i>
-                    </div>
-                    <div class="row">
-                        <?php
+        <!-- Senarai mengikut negeri -->
+        <?php foreach ($state as $value) { ?>
+            <div class="mv-state">
+                <div class="mv-state__head">
+                    <span class="mv-sec-head__eyebrow">Negeri</span>
+                    <h2><?= Html::encode(strtoupper($value->state)) ?></h2>
+                </div>
 
-                        foreach ($agen1[$i] as $user) { ?>
-                            <div class="col-lg-3 col-sm-6 sm-mb-30">
-                                <div class="team team-hover">
-                                    <div class="team-description">
-                                        <div class="team-info"><br>
-                                            <h5><a href="#"><?= $user->name ?></a></h5>
+                <?php foreach ($levels as $levelId => $level) { ?>
+                    <?php
+                    if ($levelId == 3) {
+                        $agents = User::find()->where("state=:state AND level_id=:level_id AND id<>1032", [':state' => $value->state, ':level_id' => $levelId])->all();
+                    } elseif ($levelId == 2) {
+                        $agents = User::find()->where('(state=:state AND level_id=:levelId) AND UPPER(name)<>"HEADQUATERS"', [':state' => $value->state, ':levelId' => $levelId])->all();
+                    } else {
+                        $agents = User::find()->where(['state' => $value->state, 'level_id' => $levelId])->all();
+                    }
+                    ?>
+                    <?php if ($agents) { ?>
+                        <div class="mv-agen__group">
+                            <h3><i class="<?= $level['icon'] ?>"></i> <?= $level['label'] ?></h3>
+                            <div class="mv-agen__grid">
+                                <?php foreach ($agents as $user) { ?>
+                                    <div class="mv-agent" data-search="<?= Html::encode(strtolower($user->name . ' ' . $user->city . ' ' . $value->state)) ?>">
+                                        <div class="mv-agent__avatar"><?= Html::encode(mb_strtoupper(mb_substr(trim($user->name), 0, 1))) ?></div>
+                                        <div class="mv-agent__info">
+                                            <strong><?= Html::encode($user->name) ?></strong>
                                             <?php if ($user->city) { ?>
-                                                <span><i class="fa fa-location"></i> &nbsp;<?= $user->city ?></span><?php } ?>
-                                        </div>
-                                        <div class="team-contact">
-                                            <span class="call"><i class="fa fa-phone"></i> &nbsp;<?= User::formatPhone($user->hp) ?></span>
-                                            <?php if ($user->email) { ?><br>
-                                                <span class="email"> <i class="fa fa-envelope"></i> <?= $user->email ?></span>
+                                                <span class="mv-agent__city"><i class="fas fa-location-dot"></i> <?= Html::encode($user->city) ?></span>
                                             <?php } ?>
+                                            <div class="mv-agent__contact">
+                                                <?php if ($user->hp) { ?>
+                                                    <a href="tel:<?= Html::encode(preg_replace('/\D+/', '', $user->hp)) ?>"><i class="fas fa-phone"></i> <?= Html::encode(User::formatPhone($user->hp)) ?></a>
+                                                <?php } ?>
+                                                <?php if ($user->email) { ?>
+                                                    <a href="mailto:<?= Html::encode($user->email) ?>"><i class="fas fa-envelope"></i> <?= Html::encode($user->email) ?></a>
+                                                <?php } ?>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
+                                <?php } ?>
                             </div>
-                        <?php }
-                        $i++; ?>
-                    </div>
-                <?php } ?>
-                <?php $agen2[$i] = User::find()->where("state=:state AND level_id=:level_id AND id<>1032", [':state' => $value->state, ':level_id' => 3])->all();
-                if ($agen2[$i]) { ?>
-                    <br>
-                    <h4><i class="fa fa-arrow-circle-right"></i> &nbsp;&nbsp;STOKIS</h4>
-                    <div class="divider divider-secondary">
-                        <i class="fas fa-chevron-down"></i>
-                    </div>
-
-                    <div class="row">
-                        <?php
-
-                        foreach ($agen2[$i] as $user) { ?>
-                            <div class="col-lg-3 col-sm-6 sm-mb-30">
-                                <div class="team team-hover">
-                                    <div class="team-description">
-                                        <div class="team-info"><br>
-                                            <h5><a href="#"><?= $user->name ?></a></h5>
-                                            <?php if ($user->city) { ?>
-                                                <span><i class="fa fa-location"></i> &nbsp;<?= $user->city ?></span><?php } ?>
-                                        </div>
-                                        <div class="team-contact">
-                                            <span class="call"><i class="fa fa-phone"></i> &nbsp;<?= User::formatPhone($user->hp) ?></span>
-                                            <?php if ($user->email) { ?><br>
-                                                <span class="email"> <i class="fa fa-envelope"></i> <?= $user->email ?></span>
-                                            <?php } ?>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php }
-                        $i++; ?>
-                    </div>
-                <?php } ?>
-                <?php $agen3[$i] = User::find()->where(['state' => $value->state, 'level_id' => 4])->all();
-                if ($agen3[$i]) { ?><br><br>
-                    <h4><i class="fa fa-arrow-circle-right"></i> &nbsp;&nbsp;MOBILE STOKIS</h4>
-                    <div class="divider divider-tertiary">
-                        <i class="fas fa-chevron-down"></i>
-                    </div>
-                    <div class="row">
-                        <?php
-                        foreach ($agen3[$i] as $user) { ?>
-                            <div class="col-lg-3 col-sm-6 sm-mb-30">
-                                <div class="team team-hover">
-                                    <div class="team-description">
-                                        <div class="team-info"><br>
-                                            <h5><a href="#"><?= $user->name ?></a></h5>
-                                            <?php if ($user->city) { ?>
-                                                <span><i class="fa fa-location"></i> &nbsp;<?= $user->city ?></span><?php } ?>
-                                        </div>
-                                        <div class="team-contact">
-                                            <span class="call"><i class="fa fa-phone"></i> &nbsp;<?= User::formatPhone($user->hp) ?></span>
-                                            <?php if ($user->email) { ?><br>
-                                                <span class="email"> <i class="fa fa-envelope"></i> <?= $user->email ?></span>
-                                            <?php } ?>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php }
-                        $i++; ?>
-                    </div>
+                        </div>
+                    <?php } ?>
                 <?php } ?>
             </div>
         <?php } ?>
 
+        <!-- CTA -->
+        <div class="mv-agen__cta">
+            <div>
+                <h2>Berminat menjadi pengedar?</h2>
+                <p>Sertai rangkaian 45,000+ pengedar Multivita di seluruh negara.</p>
+            </div>
+            <a href="<?= Url::to(['site/signup']) ?>" class="mv-btn mv-btn--lime">
+                Daftar Pengedar <i class="fas fa-arrow-right"></i>
+            </a>
+        </div>
+
     </div>
 </section>
+
+<script>
+    (function() {
+        var input = document.getElementById('mvAgenSearch');
+        var empty = document.getElementById('mvAgenEmpty');
+        if (!input) return;
+
+        input.addEventListener('input', function() {
+            var q = input.value.trim().toLowerCase();
+            var anyVisible = false;
+
+            document.querySelectorAll('.mv-agent').forEach(function(card) {
+                var match = !q || (card.dataset.search || '').indexOf(q) !== -1;
+                card.style.display = match ? '' : 'none';
+                if (match) anyVisible = true;
+            });
+
+            document.querySelectorAll('.mv-agen__group').forEach(function(group) {
+                var visible = group.querySelectorAll('.mv-agent:not([style*="none"])').length > 0;
+                group.style.display = visible ? '' : 'none';
+            });
+
+            document.querySelectorAll('.mv-state').forEach(function(state) {
+                var visible = state.querySelectorAll('.mv-agen__group:not([style*="none"])').length > 0;
+                state.style.display = visible ? '' : 'none';
+            });
+
+            if (empty) empty.style.display = anyVisible ? 'none' : '';
+        });
+    })();
+</script>
